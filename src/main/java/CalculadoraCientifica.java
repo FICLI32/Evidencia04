1b789d682da28303ebc86c7e9f4f531f7328c558
Fabunu <f.cheuquelaf01@ufromail.cl>
2024-09-09 15:27:54 -0300

    importacion de la clase cuadrado

---
+++ src/main/java/CalculadoraCientifica.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CalculadoraCientifica {
+   //implementa una clase cuadrado para no confundir entre codigos
+    static class Cuadrado {
+        public static double calcularPerimetro(double lado) {
+            return 4*lado;
+        }
+
+        public static double calcularArea(double lado) {
+            return lado*lado;
+        }
+     
+    }
+}
+
