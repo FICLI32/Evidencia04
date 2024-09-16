f5c549b9f22009e2aeead7769623e6f6b86b1067
Pancho <f.gatica06@ufromail.cl>
2024-09-16 20:42:05 -0300

    Correccion codigo

--- src/calculadoraCientifica.java
+++ src/calculadoraCientifica.java
@@ -2,99 +2,10 @@ import java.util.Scanner;
 
 public class calculadoraCientifica {
     public static void main (String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            menu(scanner);
-        }
-    }
-
-    public static void menu(Scanner scanner) {
-        int opcion;
-        do {
-            mostrarMenu();
-            opcion = leerOpcion(scanner);
-            ejecutarOpcion(opcion, scanner);
-        } while (opcion != 5);
-    }
-
-    public static void mostrarMenu() {
-        System.out.println("╔═════════════════════════════════╗");
-        System.out.println("║  Menu Calculadora Cientifica    ║");
-        System.out.println("║           (Aritmética)          ║");
-        System.out.println("╠═════════════════════════════════╣");
-        System.out.println("║ 1. Mayor                        ║");
-        System.out.println("║ 2. Menor                        ║");
-        System.out.println("║ 3. Potencia                     ║");
-        System.out.println("║ 4. Porcetaje                    ║");
-        System.out.println("║ 5. Salir                        ║");
-        System.out.println("╚═════════════════════════════════╝");
-        System.out.print("Selecione una opcion: ");
-    }
-
-    public static int leerOpcion(Scanner scanner) {
-        int opcion = 0;
-        while (true) {
-            if(scanner.hasNextInt()) {
-                opcion = scanner.nextInt();
-                if (opcion >= 1 && opcion <=5) {
-                    break;
-                } else {
-                    System.out.println("Opcion invalida, ingrese nuevamente");
-                    scanner.next();
-                }
-            } else {
-                System.out.println("Entrada no valida, ingrese nuevamente");
-                scanner.next();
-            }
-        }
-        scanner.nextLine();
-        return opcion;
-    }
-
-    public static void ejecutarOpcion(int opcion, Scanner scanner) {
-        switch (opcion) {
-            case 1 -> {
-                System.out.println("Calcular el mayor entre dos números. ");
-                System.out.println("Ingrese el primer número: ");
-                double primerNumero = scanner.nextDouble();
-                System.out.println("Ingrese el segundo número: ");
-                double segundoNumero = scanner.nextDouble();
-                System.out.println("El mayor de los dos es:" + mayor(primerNumero, segundoNumero));;
-            }
-
-            case 2 -> {
-                System.out.println("Calcular el menor entre dos números. ");
-                System.out.println("Ingrese el primer número: ");
-                double primerNumero = scanner.nextDouble();
-                System.out.println("Ingrese el segundo número: ");
-                double segundoNumero = scanner.nextDouble();
-                System.out.println("El menor de los dos es:" + menor(primerNumero, segundoNumero));;
-            }
-
-            case 3 -> {
-                System.out.println("Calcular potencia. ");
-                System.out.println("Ingrese la base: ");
-                double base = scanner.nextDouble();
-                System.out.println("Ingrese el exponente: ");
-                double exponente = scanner.nextDouble();
-                System.out.println("La potencia de: " + base + "elevado a:" + exponente + "= " + potencia(base, exponente));
-
-            }
-
-            case 4 -> {
-                System.out.println("Calcular porcentaje. ");
-                System.out.println("Ingrese número: ");
-                double numero = scanner.nextDouble();
-                System.out.println("ingrese porcentaje: ");
-                double porcentaje = scanner.nextDouble();
-                System.out.println("El: " + porcentaje + "% de: " + numero + "= " + calcularPorcentaje(numero, porcentaje));
-            }
-
-            case 5 -> {
-                System.out.println("Saliendo del programa...");
-            }
-
-            default -> System.out.println("Opcion no valida, intente de nuevo");
-        }
+        System.out.println("Maximo entre 2 y 3:"+ mayor(2,3));
+        System.out.println("Menor entre 6 y 3:"+ menor(6,3));
+        System.out.println("Potencia de 3 a 3:"+ potencia(3,3));
+        System.out.println("El 20% de 50:"+ porcentaje(50,20));
     }
 
     public static double mayor(double a, double b) {
@@ -109,7 +20,7 @@ public class calculadoraCientifica {
         return Math.pow(base, exponente);
     }
 
-    public static double calcularPorcentaje(double numero, double porcentaje) {
+    public static double porcentaje(double numero, double porcentaje) {
         return (numero * porcentaje) / 100;
     }
 }
